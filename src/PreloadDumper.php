4b376c63448271a536dfbe1146204a1ac25b7c8d
MartkCz <martk494@gmail.com>
2023-01-31 18:56:07 +0100

    initial commit

---
+++ src/PreloadDumper.php
@@ -0,0 +1,143 @@
+<?php declare(strict_types = 1);
+
+namespace WebChemistry\Preloader;
+
+use Composer\Autoload\ClassLoader;
+use Nette\Utils\FileSystem;
+use Nette\Utils\Json;
+use Stringable;
+
+final class PreloadDumper
+{
+
+	/** @var array<class-string, string> */
+	private array $classMap;
+
+	/** @var array<string, string> */
+	private array $noMatch = [];
+
+	/** @var array<class-string, class-string> */
+	private array $classes = [];
+
+	public function __construct(ClassLoader $loader)
+	{
+		/** @var array<class-string, string> $classMap */
+		$classMap = $loader->getClassMap();
+
+		$this->classMap = $classMap;
+	}
+
+	public function addLegacyFile(string $file): void
+	{
+		$contents = FileSystem::read($file);
+
+		// function return types
+		if (preg_match_all('#public function \w+\([^)]*\): ([a-zA-Z0-9\\\\_]+)#', $contents, $matches)) {
+			foreach ($matches[1] as $class) {
+				$this->preloadClass($class);
+			}
+		}
+
+		// new ...
+		if (preg_match_all('#new ([a-zA-Z0-9\\\\_]+)#', $contents, $matches)) {
+			foreach ($matches[1] as $class) {
+				$this->preloadClass($class);
+			}
+		}
+
+		// implements ...
+		if (preg_match_all('#implements ([a-zA-Z0-9\\\\_]+)#', $contents, $matches)) {
+			foreach ($matches[1] as $class) {
+				$this->preloadClass($class);
+			}
+		}
+	}
+
+	/**
+	 * @param iterable<string|Stringable> $files
+	 */
+	public function addFiles(iterable $files): self
+	{
+		foreach ($files as $file) {
+			$this->addFile((string) $file);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * @param iterable<string|Stringable> $files
+	 */
+	public function addLegacyFiles(iterable $files): self
+	{
+		foreach ($files as $file) {
+			$this->addLegacyFile((string) $file);
+		}
+
+		return $this;
+	}
+
+	public function addFile(string|Stringable $file): self
+	{
+		if (preg_match_all('#^use ([a-zA-Z\\\\0-9_]+)(?:\s+as\s+[a-zA-Z\\\\0-9_]+)?;#m', FileSystem::read((string) $file), $matches)) {
+			/** @var class-string $class */
+			foreach ($matches[1] as $class) {
+				$this->classes[$class] = $class;
+			}
+		}
+
+		return $this;
+	}
+
+	/**
+	 * @param class-string $class
+	 */
+	public function preloadClass(string $class): void
+	{
+		if (isset($this->classes[$class])) {
+			return;
+		}
+
+		foreach ($this->noMatch as $pattern) {
+			if (preg_match($pattern, $class)) {
+				return;
+			}
+		}
+
+		$file = $this->classMap[$class] ?? null;
+
+		if ($file !== null) {
+			$this->classes[$class] = $class;
+		}
+	}
+
+	public function exclude(string $pattern): void
+	{
+		$this->noMatch[$pattern] = $pattern;
+	}
+
+	public function preload(string $pattern): void
+	{
+		foreach (array_keys($this->classMap) as $class) {
+			if (preg_match($pattern, $class)) {
+				$this->preloadClass($class);
+			}
+		}
+	}
+
+	/**
+	 * @return class-string[]
+	 */
+	public function getClasses(): array
+	{
+		return array_values($this->classes);
+	}
+
+	public function dumpToFile(string $location): void
+	{
+		FileSystem::write($location, Json::encode([
+			'classes' => $this->getClasses(),
+		], Json::PRETTY));
+	}
+
+}
---
+++ src/Preloader.php
@@ -0,0 +1,96 @@
+<?php declare(strict_types = 1);
+
+namespace WebChemistry\Preloader;
+
+use Composer\Autoload\ClassLoader;
+use Nette\Utils\FileSystem;
+use Nette\Utils\Json;
+
+final class Preloader
+{
+
+	/** @var string[] */
+	private array $classMap;
+
+	public function __construct(
+		ClassLoader $classLoader,
+		private string $jsonFile,
+	)
+	{
+		$this->checkEnvironment();
+
+		$this->classMap = $classLoader->getClassMap();
+		$map = Json::decode(FileSystem::read($this->jsonFile), Json::FORCE_ARRAY);
+
+		if (!is_array($map)) {
+			$this->invalidJson();
+		}
+
+		$this->loadClasses($map);
+		$this->loadFiles($map);
+	}
+
+	/**
+	 * @param mixed[] $map
+	 */
+	private function loadClasses(array $map): void
+	{
+		$classes = $map['classes'] ?? null;
+
+		if (!is_array($classes)) {
+			$this->invalidJson();
+		}
+
+		foreach ($classes as $class) {
+			$file = $this->classMap[$class] ?? null;
+
+			include_once $file;
+		}
+	}
+
+	/**
+	 * @param mixed[] $map
+	 */
+	private function loadFiles(array $map): void
+	{
+		$files = $map['files'] ?? null;
+
+		if (!is_array($files)) {
+			$this->invalidJson();
+		}
+
+		/** @var string $file */
+		foreach ($files as $file) {
+			opcache_compile_file($file);
+		}
+	}
+
+	private function invalidJson(): never
+	{
+		echo sprintf('Json %s is not in expected format.', $this->jsonFile);
+
+		die(1);
+	}
+
+	private function checkEnvironment(): void
+	{
+		if (!ini_get('opcache.enable')) {
+			echo 'Opcache is not available.';
+
+			die(1);
+		}
+
+		if (!function_exists('opcache_compile_file')) {
+			echo 'Opcache function opcache_compile_file is not available.';
+
+			die(1);
+		}
+
+		if ('cli' === PHP_SAPI && !ini_get('opcache.enable_cli')) {
+			echo 'Opcache is not enabled for CLI applications.';
+
+			die(1);
+		}
+	}
+
+}
